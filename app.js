a575e4dbff7890ca776ffe7e5f1fbc8d9b5d5843
Iliyan Ivanov <iliyan.pl.ivanov@gmail.com>
2017-11-16 17:45:24 +0200

    Add new file arr.js. Update of index.html.

--- app.js
+++ app.js
@@ -20,16 +20,21 @@ console.log(myArray);
 
 // Objects
 var a = 1;
-var myObject = {course1:"Javascript1", course2:"Javascript2", price:200, title:"Intro to Objects"};
+var myObject = {
+    course1: "Javascript1",
+    course2: "Javascript2",
+    price: 200,
+    title: "Intro to Objects"
+};
 
 console.log(myObject);
-console.log(myObject["course"+a]);
+console.log(myObject["course" + a]);
 a++;
-console.log(myObject["course"+a]);
+console.log(myObject["course" + a]);
 console.log(myObject.course1);
 
 //Functions
-function myFun(a="default"){
+function myFun(a = "default") {
     // contents of the Function
     console.log(a + " function was run");
     //myFun2();
@@ -45,10 +50,24 @@ myFun();
 
 
 var b = 10;
-function myFunc(a=3){
-    console.log((a * b) + " function was run");
+var c = 1; //globally declared
+
+function myFunc(a = 3) {
+    var c = 100; //locally declared for the function
+    c = 100; //changes the global value of the var whitin the function
+   // console.log((a * b) + " function was run");
+   // console.log(c); //output the locally declared value of var c
+    return (a * b);
 }
 
 myFunc();
 myFunc(4);
 myFunc(6);
+console.log(c); //output the globally declared value of var c
+
+var d = myFunc(5); // setting it as 50 - a=5 b=10 myFunc=a*b d=50
+console.log(d);
+d += myFunc(6); // adding 60 --- d=50 myFunc=a*b a=6 b=10 myFunc=a*b=60 d+= (50+60)=110
+console.log(d);
+d += myFunc(); // adding 30 --- d=110 myFunc=a*b a=3 (from where myFunc was initially declared a=3) b=10 -> myFunc=30
+console.log(d);
---
+++ arr.js
@@ -0,0 +1,17 @@
+var myArray = ["one", "two", 3, 4, "five", "six", 7, 8, 9];
+console.log(myArray.sort());
+console.log(myArray.reverse());
+
+myArray.push("ten");
+console.log(myArray);
+myArray.pop();
+console.log(myArray);
+var a = myArray.shift();
+console.log(a);
+console.log(myArray);
+myArray.unshift("ten");
+console.log(myArray);
+myArray.splice(0,2);
+console.log(myArray);
+myArray.splice(3,0,"added 1", "added 2");
+console.log(myArray);
